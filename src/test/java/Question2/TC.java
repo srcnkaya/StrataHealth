b8814c1966675ce14949457daf0d7f5669b7b07e
sercankaya88 <sercan.kaya@outlook.com>
2022-02-12 21:49:54 -0500

    Assignment

--- src/test/java/Question2/TC.java
+++ src/test/java/Question2/TC.java
@@ -15,17 +15,10 @@ public class TC {
         Util.setup("https://common.olemiss.edu/_js/datatables/examples/data_sources/js_array.html");
 
         WebElement dropdown = Util.driver.findElement(By.xpath("//select[@name='example_length']"));
-        WebElement browserClick = Util.driver.findElement(By.xpath("//th[@class='sorting']"));
-        WebElement engineClick = Util.driver.findElement(By.xpath("//th[@class='sorting_asc']"));
-        WebElement platformClick = Util.driver.findElement(By.xpath("//th[.='Platform']"));
-        WebElement versionClick = Util.driver.findElement(By.xpath("//th[.='Version']"));
-        WebElement gradeClick = Util.driver.findElement(By.xpath("//th[.='Grade']"));
         Util.dropDown(dropdown, "100");
 
         Util.headers(By.xpath("//th[.='Engine']"));
         Util.ascending(By.className("sorting_1"));
-////        browserClick.click();
-////        Util.hardWait(2);
         Util.headers(By.xpath("//th[.='Browser']"));
         Util.ascending(By.xpath("//td[@class='sorting_1']/following-sibling::td[1]"));
         Util.headers(By.xpath("//th[.='Platform']"));
